340339f58cc9b75528086868dc09b0cda847de9b
Gentrit Abazi <gentritabazi01@gmail.com>
2020-11-20 11:09:48 +0100

    Added LaravelServiceProvider

---
+++ src/Providers/LaravelServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace one2tek\larasheets\Providers;
+
+use Illuminate\Support\ServiceProvider as BaseProvider;
+
+class LaravelServiceProvider extends BaseProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(
+            __DIR__. '../../larasheets/larapi.php',
+            'larasheets'
+        );
+    }
+
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__. '/../Config/larasheets.php' => config_path('larasheets.php'),
+        ]);
+    }
+}
